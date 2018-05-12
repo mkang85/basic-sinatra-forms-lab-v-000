2317dc3d8fa9b6ddd2fd023aa832eb64ac8f0f6e
Michael Kang <michaelkang85@gmail.com>
2018-05-12 19:39:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,4 +6,14 @@ get '/newteam' do
   erb :newteam
 end
 
+post '/team' do
+  @team_name = params['name']
+  @coach = params['coach']
+  @pg = params['pg']
+  @sg = params['sg']
+  @sf = params['sf']
+  @pf = params['pf']
+  @c = params['c']
+end
+
 end
